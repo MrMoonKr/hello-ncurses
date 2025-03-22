5d9b1a0554bbd2358af07d70fa2be3eea46112b5
Mr.Moon <MrMoonKr@gmail.com>
2025-03-22 23:34:30 +0900

    chapter02 ...

---
+++ src/chapter02/02_01_skeleton.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <ncurses/ncurses.h>
+
+int main()
+{
+    initscr(); /* Initialize Ncurses */
+
+    /* other programming */
+
+    endwin(); /* close Ncurses */
+    return 0;
+}
---
+++ src/chapter02/02_07_yourname.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <ncurses/ncurses.h>
+
+int main()
+{
+    char first[16];
+    char last[16];
+
+    initscr();
+    addstr("What is your first name? ");
+    refresh();
+    getnstr(first, 15);
+
+    addstr("What is your last name? ");
+    refresh();
+    getnstr(last, 15);
+
+    printw("Pleased to meet you, %s %s!", first, last);
+    refresh();
+    getch();
+
+    endwin();
+    return 0;
+}
